5a91717f71681dce57036f0654c1372dc4e46467
EpicMisterB <45112325+EpicMisterB@users.noreply.github.com>
2020-05-26 19:05:59 -0500

    need to debug extending circle class

--- main.js
+++ main.js
@@ -12,11 +12,55 @@ var polygon = L.polygon([
   [54.069368, -4.769606],
 ]).addTo(map);
 
-var circle = L.circle([54.069122, -4.769456], 2, {
+var DraggableCircle = L.Circle.extend({
+  initialize: function (name, options) {
+    this.name = name;
+    L.setOptions(this, options);
+    this.setRadius(2);
+    this.setLatLng([54.069223, -4.768973]);
+    this.on({
+      mousedown: function () {
+        map.on("mousemove", this.moveObject);
+      },
+      click: function () {
+        map.off("mousemove", this.moveObject);
+        map.dragging.enable();
+      },
+    });
+    this.addTo(map);
+  },
+
+  moveObject: function (event) {
+    L.Circle.prototype.setLatLng.call(this, event.latlng);
+    map.dragging.disable();
+  },
+});
+
+var testCircle = new DraggableCircle("Draggable", {
+  color: "red",
+  fillColor: "#f03",
+  fillOpacity: 1,
+  interactive: true,
+});
+
+/* var circle = L.circle([54.069122, -4.769456], 2, {
   color: "red",
   fillColor: "#f03",
   fillOpacity: 1,
   interactive: true,
 }).addTo(map);
 
-circle.on("click");
+circle.on({
+  mousedown: function () {
+    map.on("mousemove", moveObject);
+  },
+  click: function () {
+    map.off("mousemove", moveObject);
+    map.dragging.enable();
+  },
+});
+
+function moveObject(event) {
+  circle.setLatLng(event.latlng);
+  map.dragging.disable();
+} */
