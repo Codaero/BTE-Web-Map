74e463877572e0fec61dd7b0b1d8e35b6945d310
EpicMisterB <45112325+EpicMisterB@users.noreply.github.com>
2020-05-27 22:11:04 -0500

    resizeable polygons

--- main.js
+++ main.js
@@ -12,56 +12,65 @@ var polygon = L.polygon([
   [54.069368, -4.769606],
 ]).addTo(map);
 
-var DraggableCircle = L.Circle.extend({
-  initialize: function (name, options) {
-    this.name = name;
-    L.setOptions(this, options);
-    this.setRadius(2);
-    this.setLatLng([54.069223, -4.768973]);
-    this.on({
-      mousedown: function () {
-        map.on("mousemove", this.moveObject);
-      },
-      click: function () {
-        map.off("mousemove", this.moveObject);
-        map.dragging.enable();
-      },
-    });
-    this.addTo(map);
-    console.log(this);
-  },
-
-  moveObject: function (event) {
-    L.Circle.prototype.setLatLng.call(this, event.latlng);
-    map.dragging.disable();
-  },
-});
-
-var testCircle = new DraggableCircle("Draggable", {
-  color: "red",
-  fillColor: "#f03",
-  fillOpacity: 1,
-  interactive: true,
-});
-
-/* var circle = L.circle([54.069122, -4.769456], 2, {
-  color: "red",
-  fillColor: "#f03",
-  fillOpacity: 1,
-  interactive: true,
-}).addTo(map);
-
-circle.on({
-  mousedown: function () {
-    map.on("mousemove", moveObject);
-  },
-  click: function () {
-    map.off("mousemove", moveObject);
+function DraggableCircle(position) {
+  let draggableCircle = L.circleMarker(position, {
+    color: "red",
+    fillColor: "#f03",
+    fillOpacity: 1,
+    interactive: true,
+    radius: 3,
+  }).addTo(map);
+  draggableCircle.parentPolygon = null;
+  draggableCircle.index = null;
+  draggableCircle.on({
+    mousedown: function () {
+      map.dragging.disable();
+      map.on("mousemove", function (e) {
+        draggableCircle.setLatLng(e.latlng);
+        draggableCircle.parentPolygon.refresh(draggableCircle.index);
+      });
+    },
+  });
+  map.on("mouseup", function (e) {
     map.dragging.enable();
-  },
-});
+    map.removeEventListener("mousemove");
+  });
 
-function moveObject(event) {
-  circle.setLatLng(event.latlng);
-  map.dragging.disable();
-} */
+  return draggableCircle;
+}
+
+var testCircle = new DraggableCircle([54.069368, -4.769606]);
+
+function ResizeablePolygon(firstPoint) {
+  let resizeablePolygon = L.polygon([firstPoint.getLatLng()], {
+    color: "green",
+    fillColor: "#f03",
+    fillOpacity: 0.5,
+  }).addTo(map);
+  firstPoint.index = 0;
+  firstPoint.parentPolygon = resizeablePolygon;
+  resizeablePolygon.points = [firstPoint];
+
+  resizeablePolygon.addPoint = function (point) {
+    point.parentPolygon = this;
+    resizeablePolygon.addLatLng(point.getLatLng());
+    this.points.push(point);
+    point.index = this.points.length - 1;
+  };
+
+  resizeablePolygon.refresh = function (index) {
+    let updatedLatLngs = resizeablePolygon.getLatLngs();
+    updatedLatLngs[0][index] = this.points[index].getLatLng();
+    this.redraw();
+  };
+
+  return resizeablePolygon;
+}
+
+var testPoly = new ResizeablePolygon(testCircle);
+
+testPoly.addPoint(new DraggableCircle([54.069465, -4.769123]));
+
+testPoly.addPoint(new DraggableCircle([54.069223, -4.768973]));
+
+testPoly.addPoint(new DraggableCircle([54.069122, -4.769456]));
