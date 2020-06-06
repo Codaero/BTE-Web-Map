0924c4828d24df08949b994afac0faa80d147283
Aditya <45124929+AffinityGH@users.noreply.github.com>
2020-06-06 16:25:39 -0500

    major styling and custom polygon functionality update
    - styled new interface
    - added support for multiple polygons
    - created crosshair
    - created tpll command
    - created new user interface

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
+//init view angle
 var map = L.map("map").setView([51.505, -0.09], 13);
 
+//adding streetmaps
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-var polygon = L.polygon([
-  [54.069122, -4.769456],
-  [54.069223, -4.768973],
-  [54.069465, -4.769123],
-  [54.069368, -4.769606],
-]).addTo(map);
-
+//draggable circle function
 function DraggableCircle(position) {
   let draggableCircle = L.circleMarker(position, {
-    color: "red",
-    fillColor: "#f03",
+    color: "#4285F4",
+    fillColor: "#4285F4",
     fillOpacity: 1,
     interactive: true,
     radius: 3,
@@ -36,28 +32,28 @@ function DraggableCircle(position) {
     map.removeEventListener("mousemove");
   });
 
-  return draggableCircle;
+  return draggableCircle; //returns the circle
 }
 
-var testCircle = new DraggableCircle([54.069368, -4.769606]);
 
+//create a new polygon using a "mother" point
 function ResizeablePolygon(firstPoint) {
   let resizeablePolygon = L.polygon([firstPoint.getLatLng()], {
-    color: "green",
-    fillColor: "#f03",
+    color: "#4285F4",
+    fillColor: "#F4B400",
     fillOpacity: 0.5,
   }).addTo(map);
   firstPoint.index = 0;
   firstPoint.parentPolygon = resizeablePolygon;
   resizeablePolygon.points = [firstPoint];
-
+  //add point function
   resizeablePolygon.addPoint = function (point) {
     point.parentPolygon = this;
     resizeablePolygon.addLatLng(point.getLatLng());
     this.points.push(point);
     point.index = this.points.length - 1;
   };
-
+  //refreshes the polygon
   resizeablePolygon.refresh = function (index) {
     let updatedLatLngs = resizeablePolygon.getLatLngs();
     updatedLatLngs[0][index] = this.points[index].getLatLng();
@@ -67,10 +63,60 @@ function ResizeablePolygon(firstPoint) {
   return resizeablePolygon;
 }
 
-var testPoly = new ResizeablePolygon(testCircle);
+var pointArray = [];
 
-testPoly.addPoint(new DraggableCircle([54.069465, -4.769123]));
 
-testPoly.addPoint(new DraggableCircle([54.069223, -4.768973]));
+//get lat and long from center of map
+function findLatLong(){
 
-testPoly.addPoint(new DraggableCircle([54.069122, -4.769456]));
+  let ctr = map.getCenter();
+  
+  let lat = Math.round(ctr.lat * 1000000) / 1000000;
+  let lon = Math.round(ctr.lng * 1000000) / 1000000;
+  
+  document.getElementById("latlong").innerHTML = "/tpll " + lat +", "+ lon;
+}
+
+//update lat and long
+map.on('moveend', findLatLong);
+
+//button code
+var index = -1;
+var motherPoints = [];
+var arrayOfArrays = [];
+var disposableArray = [];
+
+
+function createNewMother()
+{
+  let ctr = map.getCenter();
+  
+  let lat = Math.round(ctr.lat * 1000000) / 1000000;
+  let lon = Math.round(ctr.lng * 1000000) / 1000000;
+
+  temp = new DraggableCircle([lat, lon]);
+  tempPoly = new ResizeablePolygon(temp);
+  
+  motherPoints.push(tempPoly);
+
+  arrayOfArrays.push(disposableArray);
+
+  index++;
+  disposableArray = [];
+}
+
+function addChildren()
+{
+  let ctr = map.getCenter();
+  
+  let lat = Math.round(ctr.lat * 1000000) / 1000000;
+  let lon = Math.round(ctr.lng * 1000000) / 1000000;
+  
+  temp = new DraggableCircle([lat, lon]);
+
+  disposableArray.push(temp);
+
+  mother = motherPoints[index];
+
+  mother.addPoint(temp);
+}
