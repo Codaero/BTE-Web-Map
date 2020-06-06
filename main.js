92093c9bfda7efe80e5d1470ca0447bbc7e47bbb
Aditya <45124929+AffinityGH@users.noreply.github.com>
2020-06-06 18:09:12 -0500

    added search bar settings
    -it works
    -clean animation

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 //init view angle
-var map = L.map("map").setView([51.505, -0.09], 13);
+var map = L.map('map', {
+  // Set latitude and longitude of the map center (required)
+  center: [40.229301, -89.121094],
+  // Set the initial zoom level, values 0-18, where 0 is most zoomed-out (required)
+  zoom: 5
+});
 
 //adding streetmaps
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -120,3 +125,19 @@ function addChildren()
 
   mother.addPoint(temp);
 }
+
+var tiles = new L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+  var searchControl = new L.esri.Controls.Geosearch().addTo(map);
+
+  var results = new L.LayerGroup().addTo(map);
+
+  searchControl.on('results', function(data){
+    results.clearLayers();
+    for (var i = data.results.length - 1; i >= 0; i--) {
+      results.addLayer(L.marker(data.results[i].latlng));
+    }
+  });
+
+setTimeout(function(){$('.pointer').fadeOut('slow');},3400);
